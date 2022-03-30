32146e5f6805edc26f0c862b0ce56d5e73c22f26
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-30 10:08:00 -0300

    states implemented

--- client/src/components/RegisterContainer.jsx
+++ client/src/components/RegisterContainer.jsx
@@ -1,13 +1,45 @@
+import { useState } from 'react';
 import './styles/RegisterContainer.css';
 
 export default function () {
+	const [game, setGame] = useState({});
+
+	const handleNewGame = (ev) => {
+		setGame((prev) => ({
+			...prev,
+			[ev.target.name]: ev.target.value,
+		}));
+		console.log(game);
+	};
+
 	return (
 		<div className='Register-container'>
 			<h2>Nostalgic Games</h2>
 
-			<input type='text' name='title' placeholder='Game...' />
-			<input type='text' name='year' placeholder='Ano...' />
-			<input type='text' name='category' placeholder='Categoria...' />
+			<input
+				type='text'
+				name='gameTitle'
+				placeholder='Game...'
+				onChange={handleNewGame}
+			/>
+			<input
+				type='text'
+				name='year'
+				placeholder='Ano...'
+				onChange={handleNewGame}
+			/>
+			<input
+				type='text'
+				name='tradeMark'
+				placeholder='Marca...'
+				onChange={handleNewGame}
+			/>
+			<input
+				type='text'
+				name='category'
+				placeholder='Categoria...'
+				onChange={handleNewGame}
+			/>
 			<button className='btn-register'>Cadastrar</button>
 		</div>
 	);

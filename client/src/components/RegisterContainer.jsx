5763b52dacfa3753c7f0568dd782660e3adbb8dc
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-30 11:37:47 -0300

    input radio rating

--- client/src/components/RegisterContainer.jsx
+++ client/src/components/RegisterContainer.jsx
@@ -1,6 +1,8 @@
 import { useState } from 'react';
 import './styles/RegisterContainer.css';
 
+import uncheckedURL from '../assets/star-unchecked.svg';
+
 export default function () {
 	const [game, setGame] = useState({});
 
@@ -43,6 +45,32 @@ export default function () {
 				placeholder='Categoria...'
 				onChange={handleNewGame}
 			/>
+
+			<fieldset className='radio-container'>
+				<legend> Classificação: </legend>
+
+				<input type='radio' name='rating' id='r1' title='1' />
+				<label htmlFor=''>
+					<img src={uncheckedURL} alt='1' />
+				</label>
+				<input type='radio' name='rating' id='r2' title='2' />
+				<label htmlFor=''>
+					<img src={uncheckedURL} alt='2' />
+				</label>
+				<input type='radio' name='rating' id='r3' title='3' />
+				<label htmlFor=''>
+					<img src={uncheckedURL} alt='3' />
+				</label>
+				<input type='radio' name='rating' id='r4' title='4' />
+				<label htmlFor=''>
+					<img src={uncheckedURL} alt='4' />
+				</label>
+				<input type='radio' name='rating' id='r5' title='5' />
+				<label htmlFor=''>
+					<img src={uncheckedURL} alt='5' />
+				</label>
+			</fieldset>
+
 			<button className='btn-register' onClick={handleRegister}>
 				Cadastrar
 			</button>

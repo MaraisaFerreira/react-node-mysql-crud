eed63b737d59f40177e2467a5e6c4e4a820213e2
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-31 12:40:02 -0300

    get data by post method

--- server/src/index.js
+++ server/src/index.js
@@ -17,9 +17,12 @@ const db = mysql.createPool({
 	database: process.env.DATABASE,
 });
 
-app.get('/', (req, res) => {
-
-	res.status(200);
+app.post('/register', (req, res) => {
+	const { title } = req.body;
+	const { year } = req.body;
+	const { trademark } = req.body;
+	const { category } = req.body;
+	const { rating } = req.body;
 });
 
 app.listen(PORT, () => {

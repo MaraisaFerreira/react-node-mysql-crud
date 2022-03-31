4fb4406573964bc272f6084049985d9c2cd05b9f
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-03-31 11:36:58 -0300

    .env & db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,11 +1,25 @@
+require('dotenv').config();
+
 const express = require('express');
-const db = require('mysql');
+const cors = require('cors');
+const mysql = require('mysql');
 
 const PORT = 3001;
+
 const app = express();
+app.use(cors());
+app.use(express.json());
+
+const db = mysql.createPool({
+	host: process.env.HOST,
+	user: process.env.USER,
+	password: process.env.PASSWORD,
+	database: process.env.DATABASE,
+});
 
 app.get('/', (req, res) => {
-	res.send('Hello World!');
+
+	res.status(200);
 });
 
 app.listen(PORT, () => {

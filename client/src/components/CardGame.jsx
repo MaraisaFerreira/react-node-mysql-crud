daaf79f9ac824b300dc7113469f082848b3c1667
maraisaferreira <ferreira.maraisacristina@gmail.com>
2022-04-03 11:32:51 -0300

    fix idgames

--- client/src/components/CardGame.jsx
+++ client/src/components/CardGame.jsx
@@ -1,7 +1,15 @@
 import './styles/CardGame.css';
 import { FaTrash, FaPen } from 'react-icons/fa';
 
-export default function ({ title, year, trademark, category, rating }) {
+export default function ({ id, title, year, trademark, category, rating }) {
+	const handleEdit = (v) => {
+		console.log('Edit', v);
+	};
+
+	const handleDelete = (v) => {
+		console.log('Delete', v);
+	};
+
 	return (
 		<div className='card-game'>
 			<h3>{title}</h3>
@@ -18,8 +26,16 @@ export default function ({ title, year, trademark, category, rating }) {
 				<span>Rating:</span> {rating} stars
 			</p>
 			<div className='card-options'>
-				<FaPen className='opt-icons pen' />{' '}
-				<FaTrash className='opt-icons trash' />
+				<FaPen
+					className='opt-icons pen'
+					onClick={(e) => {
+						handleEdit(id);
+					}}
+				/>
+				<FaTrash
+					className='opt-icons trash'
+					onClick={(e) => handleDelete(id)}
+				/>
 			</div>
 		</div>
 	);
--- client/src/components/GamesContainer.jsx
+++ client/src/components/GamesContainer.jsx
@@ -23,6 +23,8 @@ export default function () {
 				{listGame.map((item) => {
 					return (
 						<CardGame
+							key={item.idgames}
+							id={item.idgames}
 							title={item.title}
 							year={item.year}
 							trademark={item.trademark}
@@ -32,36 +34,6 @@ export default function () {
 					);
 				})}
 			</div>
-
-			{/* <table className='list'>
-				<thead>
-					<tr>
-						<th>Title</th>
-						<th>Year</th>
-						<th>Trademark</th>
-						<th>Category</th>
-						<th>Rating</th>
-						<th>Options</th>
-					</tr>
-				</thead>
-				<tbody>
-					{listGame.map((item) => {
-						return (
-							<tr key={item.id}>
-								<td>{item.title}</td>
-								<td>{item.year}</td>
-								<td>{item.trademark}</td>
-								<td>{item.category}</td>
-								<td>{item.rating} stars</td>
-								<td>
-									<FaPen className='icons pen' />{' '}
-									<FaTrash className='icons trash' />
-								</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table> */}
 		</main>
 	);
 }
